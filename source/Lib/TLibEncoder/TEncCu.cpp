f0abad054d71fcfc35017011804689d3517e7fb9
PharrellWANG <15113029g@connect.polyu.hk>
2017-09-16 23:09:28 +0800

    modification for split and nonsplit data collection [clean up the codes]

--- source/Lib/TLibEncoder/TEncCu.cpp
+++ source/Lib/TLibEncoder/TEncCu.cpp
@@ -1657,17 +1657,21 @@ Void TEncCu::xEncodeCU( TComDataCU* pcCU, UInt uiAbsPartIdx, UInt uiDepth )
     // ------------------------------------------------------------------------------------------------------
     // start ------------------------------------------------------------------------------------------------------
     // ------------------------------------------------------------------------------------------------------
-	  try {
-      // if DIS not used:
-      if (iDISFlag == 0) {
-        char szFileName[FILENAME_MAX];
-//        sprintf(szFileName, "/Users/pharrell_wang/htm_debug_files/data_exported_for_homo_split/mixed_data_%d.csv", uiDepth);
-        // get current working directory and append the name.
-        GetCurrentDir(szFileName, sizeof(szFileName));
-        char partOfName[FILENAME_MAX];
-        sprintf(partOfName, "/data_exported_for_homo_split/mix_data_%d.csv", uiDepth);
-        strcat(szFileName,partOfName);
-        // get current working directory and append the name.
+    if (iPartNum == 1) {
+      try {
+        char szFileName[1024];
+        // get cwd pharrell begin
+//              char buffer[SIZE];
+//              char *answer = getcwd(buffer, sizeof(buffer));
+//              std::string s_cwd;
+//              if (answer) {
+//                  s_cwd = answer;
+//              }
+        // get cwd pharrell end
+        sprintf(szFileName, "/Users/Pharrell_WANG/XcodeProj/for_HTM_debugging/data_exported/mixed_data_%d.csv", uiDepth);
+//              sprintf(szFileName, "/Users/zwapimachk/data_collecting/undo_dancer_1920x1088/data_exported/mixed_data_%d.csv", uiDepth);
+//              sprintf(szFileName, "/data_exported/mixed_data_%d.csv", uiDepth);
+//              std::string fileName = s_cwd + szFileName;
         csvfile csv(szFileName);
         // write CU original pixels
         for (y = 0; y < uiCuSize; y++) {
@@ -1676,24 +1680,112 @@ Void TEncCu::xEncodeCU( TComDataCU* pcCU, UInt uiAbsPartIdx, UInt uiDepth )
           pOrgPel += iStride;
         }
         // write CU mode and intra prediction direction
-//        csv << iDISFlag;
-//        csv << iDISType;
-        if (iPartNum == 1) {
-          csv << 0; // HOMO
-        } else if (iPartNum == 4){
-          csv << 1; // SPLIT
-        } else {
-          std::cout << "error occurred, please stop the execution and inspect the codes...." << std::endl;
+//              csv << iDISFlag;
+//              csv << iDISType;
+//              csv << iPartNum;
+//              for (j = 0; j < 4; j++)
+        if (iDir[0]<35){
+          csv << iDir[0];
+        } else if (iDir[0] == 37){
+          csv << 35;
+          assert ( typeid(iDir[0]).name() == typeid(37).name() );
+        } else if (iDir[0] == 38){
+          csv << 36;
+          assert ( typeid(iDir[0]).name() == typeid(37).name() );
         }
-//        csv << iPartNum;
-//        for (j = 0; j < 4; j++)
-//          csv << iDir[j];
+
         csv << endrow;
       }
-	  }
-	  catch (const std::exception &ex) {
-		  std::cout << "Exception was thrown: " << ex.what() << std::endl;
-	  }
+      catch (const std::exception &ex) {
+        std::cout << "Exception was thrown: " << ex.what() << std::endl;
+      }
+    } else if (iPartNum == 4) {
+      try {
+        UInt data_collecting_depth;
+
+        UInt yStartPos;
+        UInt yEndPos;
+        UInt xStartPos;
+        UInt xEndPos;
+
+        data_collecting_depth = uiDepth + 1;
+        char szFileName[1024];
+        // get cwd pharrell begin
+//              char buffer[SIZE];
+//              char *answer = getcwd(buffer, sizeof(buffer));
+//              std::string s_cwd;
+//              if (answer) {
+//                  s_cwd = answer;
+//              }
+        // get cwd pharrell end
+        sprintf(szFileName, "/Users/Pharrell_WANG/XcodeProj/for_HTM_debugging/data_exported/mixed_data_%d.csv", uiDepth);
+//              sprintf(szFileName, "/Users/zwapimachk/data_collecting/undo_dancer_1920x1088/data_exported/mixed_data_%d.csv", data_collecting_depth);
+//              sprintf(szFileName, "/data_exported/mixed_data_%d.csv", data_collecting_depth);
+//              std::string fileName = s_cwd + szFileName;
+//              csvfile csv(fileName);
+        csvfile csv(szFileName);
+        // write CU original pixels
+
+        for(j=0; j<iPartNum; j++) {
+          const UInt size_pha = maxCUWidth >> data_collecting_depth;
+
+          if (j == 0) {
+
+            yStartPos = 0;
+            yEndPos = size_pha;
+
+            xStartPos = 0;
+            xEndPos = size_pha;
+
+          } else if (j == 1) {
+
+            yStartPos = 0;
+            yEndPos = size_pha;
+
+            xStartPos = size_pha;
+            xEndPos = size_pha*2;
+
+          } else if (j == 2) {
+
+            yStartPos = size_pha;
+            yEndPos = size_pha * 2;
+
+            xStartPos = 0;
+            xStartPos = size_pha;
+
+          } else if (j == 3) {
+
+            yStartPos = size_pha;
+            yEndPos = size_pha * 2;
+
+            xStartPos = size_pha;
+            xEndPos = size_pha * 2;
+
+          }
+
+          for (y = yStartPos; y < yEndPos; y++) {
+            for (x = xStartPos; x < xEndPos; x++)
+              csv << pOrgPel[x];
+            pOrgPel += iStride;
+          }
+          // write CU mode and intra prediction direction
+          if (iDir[j]<35){
+            csv << iDir[j];
+          } else if (iDir[j] == 37){
+            csv << 35;
+            assert ( typeid(iDir[j]).name() == typeid(37).name() );
+          } else if (iDir[j] == 38){
+            csv << 36;
+            assert ( typeid(iDir[j]).name() == typeid(37).name() );
+          }
+
+          csv << endrow;
+        }
+      }
+      catch (const std::exception &ex) {
+        std::cout << "Exception was thrown: " << ex.what() << std::endl;
+      }
+    }
     // ------------------------------------------------------------------------------------------------------
     // end ------------------------------------------------------------------------------------------------------
     // ------------------------------------------------------------------------------------------------------
